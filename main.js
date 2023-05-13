69fd6f148f553b6c2c2ce7810d66d2e9c2f7de2e
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-05-13 18:49:19 +0530

    css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function myFunction() {
+function pwdgen() {
   let name = document.getElementById("name").value;
   let number = document.getElementById("numb").value;
-  let dob = document.getElementById("dob").value;
-  let text=CryptoJS.MD5(name+dob+number);
+  // let dob = document.getElementById("dob").value;
+  let text = CryptoJS.MD5(name + number);
   document.getElementById("demo").innerHTML = `YOUR PWD IS ${text}`;
 }
